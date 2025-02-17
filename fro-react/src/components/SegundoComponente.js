3843540ea7ca2575f6ed613599c7aeb9a49c0234
Fernand0Moreno <kmorenos2@est.ups.edu.ec>
2025-02-17 18:11:53 -0500

    Primer commit

---
+++ fro-react/src/components/SegundoComponente.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class SegundoComponente extends PureComponent {
+  render() {
+    return (
+      <div>SegundoComponente</div>
+    )
+  }
+}
+
+//rpc para crear todo el codigo de una vez
